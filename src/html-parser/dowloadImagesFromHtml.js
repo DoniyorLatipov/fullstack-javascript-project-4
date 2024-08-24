9597a581c22d2fc8f562aa3e9bb8479a1df7ea93
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-08-24 19:32:02 +0500

    refactor: remove unused import (lodash)

--- src/html-parser/dowloadImagesFromHtml.js
+++ src/html-parser/dowloadImagesFromHtml.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 import fs from 'fs/promises';
-import _ from 'lodash';
 import path from 'path';
 import createFilename from '../createFilename.js';
 
