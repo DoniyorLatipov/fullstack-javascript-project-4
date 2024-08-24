60de0f733193e37617152a6fc19d933155105f9d
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-08-24 17:44:36 +0500

    test: add test for new module getting html with local sources

---
+++ __tests__/makeLocalHtml.test.js
@@ -0,0 +1,30 @@
+import makeLocalHtml from '../src/html-parser/makeLocalHtml.js';
+import prettierHtml from '../src/html-parser/prettierHtml.js';
+
+import { fileURLToPath } from 'url';
+import fs from 'fs/promises';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+let expected;
+
+beforeAll(async () => {
+  const htmlData = await fs.readFile(getFixturePath('after-ru-hexlet-io-courses.html'), 'utf-8');
+  expected = await prettierHtml(htmlData);
+});
+
+const assetsDirname = 'ru-hexlet-io-courses_files';
+const url = 'https://ru.hexlet.io/courses';
+let data;
+
+beforeEach(async () => {
+  data = await fs.readFile(getFixturePath('before-ru-hexlet-io-courses.html'), 'utf-8');
+});
+
+test('makeLocalHtml', async () => {
+  const localHtml = await makeLocalHtml(data, url, assetsDirname);
+  expect(localHtml).toBe(expected);
+});
