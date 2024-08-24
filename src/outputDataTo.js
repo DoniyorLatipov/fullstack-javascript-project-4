c1bd3edbfc9f78a84a921ada3dbb00f3b3d2c2b4
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-08-24 18:15:37 +0500

    feat: create a new module to combine the parsing modules into one
    function for receiving response with html from web page and output direction:
    1) makeLocalHtml with local src for images
    2) write html to output direction
    3) make direction for assets
    4) dowloadImagesFromHtml to assets directory

---
+++ src/outputDataTo.js
@@ -0,0 +1,24 @@
+import createFilename from './createFilename.js';
+import makeLocalHtml from './html-parser/makeLocalHtml.js';
+import fs from 'fs/promises';
+import path from 'path';
+import dowloadImagesFromHtml from './html-parser/dowloadImagesFromHtml.js';
+
+export default function outputDataTo(response, outputDir) {
+  const { data } = response;
+  const { url } = response.config;
+
+  const htmlFilename = createFilename(url, '.html');
+  const htmlFilepath = path.join(outputDir, htmlFilename);
+
+  const assetsDirname = createFilename(url, '_files');
+  const assetsDirpath = path.join(outputDir, assetsDirname);
+
+  return fs
+    .stat(outputDir)
+    .then(() => makeLocalHtml(data, url, assetsDirname))
+    .then((localedHtml) => fs.writeFile(htmlFilepath, localedHtml))
+    .then(() => fs.mkdir(assetsDirpath))
+    .then(() => dowloadImagesFromHtml(data, url, assetsDirpath))
+    .then(() => htmlFilepath);
+}
