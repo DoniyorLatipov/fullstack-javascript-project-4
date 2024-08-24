c6825222fb227ab477cef3925084a93990347794
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-08-24 17:36:56 +0500

    test: rename and change the mechanics of test for createFilename

---
+++ __tests__/createFilename.test.js
@@ -0,0 +1,10 @@
+import createFilename from '../src/output/createFilename.js';
+
+describe('createOutputPath', () => {
+  const table = ['https://t.est/c_p=t', 'http://t.est/c_p=t', '/t.est/c_p=t', '   t.est/c_p=t   '];
+  const expected = 't-est-c-p-t.html';
+
+  test.each(table)('%s', (url) => {
+    expect(createFilename(url, '.html')).toBe(expected);
+  });
+});
