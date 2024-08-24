9b18b88363a2ba210789e03bd59f2ae3d8f33d9a
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-08-24 18:42:49 +0500

    test: fix path for the tested module

--- __tests__/createFilename.test.js
+++ __tests__/createFilename.test.js
@@ -1,4 +1,4 @@
-import createFilename from '../src/output/createFilename.js';
+import createFilename from '../src/createFilename.js';
 
 describe('createOutputPath', () => {
   const table = ['https://t.est/c_p=t', 'http://t.est/c_p=t', '/t.est/c_p=t', '   t.est/c_p=t   '];
