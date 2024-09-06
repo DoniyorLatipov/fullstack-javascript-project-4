cd27deadc04807a82986bd7ee2d92a66189d308f
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-09-06 21:34:35 +0500

    refactor: rename and reroute imported variables

--- src/outputDataTo.js
+++ src/outputDataTo.js
@@ -1,8 +1,8 @@
 import fs from 'fs/promises';
 import path from 'path';
-import createFilename from './createFilename.js';
+import createFilename from './url/createFilename.js';
 import makeLocalHtml from './html-parser/makeLocalHtml.js';
-import dowloadImagesFromHtml from './html-parser/dowloadImagesFromHtml.js';
+import dowloadAssetsFromHtml from './asset-downloader/index.js';
 
 export default function outputDataTo(response, outputDir) {
   const { data } = response;
@@ -19,6 +19,6 @@ export default function outputDataTo(response, outputDir) {
     .then(() => makeLocalHtml(data, url, assetsDirname))
     .then((localedHtml) => fs.writeFile(htmlFilepath, localedHtml))
     .then(() => fs.mkdir(assetsDirpath))
-    .then(() => dowloadImagesFromHtml(data, url, assetsDirpath))
+    .then(() => dowloadAssetsFromHtml(data, url, assetsDirpath))
     .then(() => htmlFilepath);
 }
