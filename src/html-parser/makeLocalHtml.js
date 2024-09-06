1611e2423ba3d11cba49a50c0bbd19220e9879a7
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-09-06 21:29:44 +0500

    feat: parsing "link" and "srcipt" tags

--- src/html-parser/makeLocalHtml.js
+++ src/html-parser/makeLocalHtml.js
@@ -1,18 +1,11 @@
 import * as cheerio from 'cheerio';
-import createFilename from '../createFilename.js';
-import splitByExtname from '../splitByExtname.js';
 import prettierHtml from './prettierHtml.js';
+import makeLocalTagAttributes from './makeLocalTagAttributes.js';
 
 export default function makeLocalHtml(data, url, assetsDirname) {
-  const $ = cheerio.load(data, { baseURI: url });
-  $('img').each((_i, img) => {
-    const globalSrc = $(img).prop('src');
-    const [noExtnameSrc, extname] = splitByExtname(globalSrc);
-    const imgFilename = createFilename(noExtnameSrc, extname);
-
-    const localSrc = `/${assetsDirname}/${imgFilename}`;
-    $(img).attr('src', localSrc);
-  });
-
-  return prettierHtml($.html());
+  return Promise.resolve(cheerio.load(data, { baseURI: url }))
+    .then(($) => makeLocalTagAttributes($, 'img', 'src', assetsDirname))
+    .then(($LocaledImgs) => makeLocalTagAttributes($LocaledImgs, 'link', 'href', assetsDirname))
+    .then(($LocaledLinks) => makeLocalTagAttributes($LocaledLinks, 'script', 'src', assetsDirname))
+    .then(($AllLocaled) => prettierHtml($AllLocaled.html()));
 }
---
+++ src/html-parser/makeLocalTagAttributes.js
@@ -0,0 +1,31 @@
+import path from 'path';
+import createFilename from '../url/createFilename.js';
+import splitByExtname from '../url/splitByExtname.js';
+import compareUrlsByHostname from '../url/compareUrlsByHostname.js';
+
+export default function makeLocalTagAttributes(CheerioAPI, tag, attribute, assetsDirname) {
+  const $ = CheerioAPI;
+  const { baseURI } = $._options;
+
+  $(`${tag}[${attribute}]`).each((_i, el) => {
+    const globalSrc = $(el).prop(attribute);
+
+    if (!compareUrlsByHostname(globalSrc, baseURI)) {
+      return;
+    }
+
+    let [noExtnameSrc, extname] = splitByExtname(globalSrc);
+
+    if (!path.extname(new URL(globalSrc).pathname)) {
+      noExtnameSrc = globalSrc;
+      extname = '.html';
+    }
+
+    const elFilename = createFilename(noExtnameSrc, extname);
+
+    const localSrc = path.join(assetsDirname, elFilename);
+    $(el).attr(attribute, localSrc);
+  });
+
+  return $;
+}
