3393149bc21ba81c666c1d8d3280602263ea2d82
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-09-10 20:16:55 +0500

    style: remove extra spaces

--- src/asset-downloader/writeAssetsToOutput.js
+++ src/asset-downloader/writeAssetsToOutput.js
@@ -7,14 +7,14 @@ export default function writeAssetsToOutput(assets, output) {
   const writtingPromises = assets.map(([src, data]) => {
     let [noExtnameSrc, extname] = splitByExtname(src);
 
-      if (!path.extname(new URL(src).pathname)) {
-        noExtnameSrc = src;
-        extname = '.html';
-      }
+    if (!path.extname(new URL(src).pathname)) {
+      noExtnameSrc = src;
+      extname = '.html';
+    }
 
-      const filename = createFilename(noExtnameSrc, extname);
-      return fs.writeFile(path.join(output, filename), data);
-    });
+    const filename = createFilename(noExtnameSrc, extname);
+    return fs.writeFile(path.join(output, filename), data);
+  });
 
   return Promise.all(writtingPromises);
 }
