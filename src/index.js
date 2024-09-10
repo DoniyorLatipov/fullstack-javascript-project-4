69433c16644e27ce5f436ee0e2fdd2a07bc8509c
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-09-10 23:13:51 +0500

    feat: error logging for axios and use process.exit to finish

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import outputDataTo from './outputDataTo.js';
 export default (url, outputDir) => {
   const parsingPromise = axios
     .get(url)
+    .catch((errorRequest) => {
+      const requestStatus = errorRequest.status ? ` (${errorRequest.status})` : '';
+      console.error(`Error: can not connect to '${errorRequest.config.url}'${requestStatus}`);
+      throw new Error();
+    })
     .then((response) => outputDataTo(response, outputDir))
-    .then((filepath) => console.log(filepath));
+    .then((filepath) => console.log(filepath))
+    .catch(() => {
+      process.exitCode = 1;
+    })
+    .then(() => process.exit());
   return parsingPromise;
 };
