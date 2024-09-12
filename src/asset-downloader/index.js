83574f232d9424a11f87be2703108512d5245334
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-09-12 13:55:03 +0500

    style: fix typo in imported module 'dowloadAs...'

--- src/asset-downloader/index.js
+++ src/asset-downloader/index.js
@@ -1,10 +1,10 @@
 import fs from 'fs/promises';
-import dowloadAllLocalAssets from './dowloadAllLocalAssets.js';
+import downloadAssetsFromHtml from './downloadAllLocalAssets.js';
 import writeAssetsToOutput from './writeAssetsToOutput.js';
 
 export default function dowloadAssetsFromHtml(html, url, output) {
   return fs
     .stat(output)
-    .then(() => dowloadAllLocalAssets(html, url))
+    .then(() => downloadAssetsFromHtml(html, url))
     .then((localAssets) => writeAssetsToOutput(localAssets, output));
 }
