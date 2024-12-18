e92dbc6d44c85e55124bd88180520fedb0b1fa0b
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-12-18 22:00:47 +0500

    refactor: change asset path localization script using Promise.all

--- src/html-parser/makeLocalHtml.js
+++ src/html-parser/makeLocalHtml.js
@@ -2,10 +2,17 @@ import * as cheerio from 'cheerio';
 import prettierHtml from './prettierHtml.js';
 import makeLocalTagAttributes from './makeLocalTagAttributes.js';
 
+const arrayRequests = [
+  ['img', 'src'],
+  ['source', 'srcset'],
+  ['link', 'href'],
+  ['script', 'src'],
+];
+
 export default function makeLocalHtml(data, url, assetsDirname) {
-  return Promise.resolve(cheerio.load(data, { baseURI: url }))
-    .then(($) => makeLocalTagAttributes({ cheerio: $, baseURI: url }, 'img', 'src', assetsDirname))
-    .then(($LocImgs) => makeLocalTagAttributes({ cheerio: $LocImgs, baseURI: url }, 'link', 'href', assetsDirname))
-    .then(($LocLinks) => makeLocalTagAttributes({ cheerio: $LocLinks, baseURI: url }, 'script', 'src', assetsDirname))
-    .then(($LocAll) => prettierHtml($LocAll.html()));
+  const $ = cheerio.load(data, { baseURI: url });
+  const localedCherioPromise = arrayRequests.reduce((acc, [tag, attribute]) => {
+    return acc.then((cheerio) => makeLocalTagAttributes({ cheerio, baseURI: url }, tag, attribute, assetsDirname));
+  }, Promise.resolve($));
+  return localedCherioPromise.then((localedCherio) => prettierHtml(localedCherio.html()));
 }
